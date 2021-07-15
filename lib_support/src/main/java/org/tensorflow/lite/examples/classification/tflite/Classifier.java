e58953bc8f75179845e6b0890f53e33f0d7fe1c7
George <farmaker47@gmail.com>
2021-07-15 11:24:07 +0300

    Fix typo

--- lib_support/src/main/java/org/tensorflow/lite/examples/classification/tflite/Classifier.java
+++ lib_support/src/main/java/org/tensorflow/lite/examples/classification/tflite/Classifier.java
@@ -366,9 +366,9 @@ public abstract class Classifier {
     // https://github.com/tensorflow/tflite-support/blob/master/tensorflow_lite_support/java/src/java/org/tensorflow/lite/support/image/ops/Rot90Op.java
     if (divisionResult == 1) {
       numRotation = 1;
-    } else if (divisioResult == 2) {
+    } else if (divisionResult == 2) {
       numRotation = 2;
-    } else if (divisioResult == 3) {
+    } else if (divisionResult == 3) {
       numRotation = 3;
     } else {
       numRotation = 0;
